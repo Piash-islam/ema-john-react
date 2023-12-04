b239e6c117502d950204fd62b503e5e3d305ee20
Piash-islam <65068589+Piash-islam@users.noreply.github.com>
2023-12-04 11:40:46 +0600

    implemented delete from cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 // import React from 'react';
 import './Header.css'
 import logo from '../../images/Logo.svg'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="1">Order</a>
-                <a href="2">Order review</a>
-                <a href="3">Manage Inventory</a>
-                <a href="4">LogIn</a>
+                <Link to="/">Shop</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/login">LogIn</Link>
             </div>
         </nav>
     );
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>LogIn page is coming</h1>
+        </div>
+    );
+};
+
+export default Login;
